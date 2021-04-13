75140237bcf181918c24ad586fc5e4d30a1e3586
hx <hxniit@126.com>
2021-04-13 21:27:40 +0800

    kafka使用

--- xin-demo/xin-commons-multi/xin-commons-multi-kafka/src/main/java/com/xin/commons/multi/kafka/consumer/Consumer.java
+++ xin-demo/xin-commons-multi/xin-commons-multi-kafka/src/main/java/com/xin/commons/multi/kafka/consumer/Consumer.java
@@ -4,6 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
@@ -22,6 +25,10 @@ public class Consumer {
      * tpoic 主题
      */
     private static final String TPOIC = "TOPIC-SDK-WELFARE-LOG";
+    /**
+     * topic groupid
+     */
+    private static final String TOPICGROUPID = "TOPIC-SDK-WELFARE-LOG";
 
     @KafkaListener(topics = {TPOIC}, containerFactory = "kafkaListener1")
     //ConsumerRecord<?, Object> record 可以为 ConsumerRecord<?, User> 确切对象
@@ -37,6 +44,30 @@ public class Consumer {
 
             } catch (Exception ex) {
                 log.error("[listen] 转换 fail: {}", ExceptionUtils.getStackTrace(ex));
+            }
+        }
+    }
+
+    /**
+     * 第二种是 指定topic ，
+     * 手动提交，需要注意，使用手动提交，配置文件enable-auto-commit: false
+     * @param record
+     * @param ack
+     * @param topic
+     */
+    @KafkaListener(topics = TPOIC, groupId = TOPICGROUPID, containerFactory = "kafkaListener1")
+    public void dataCollectListener(ConsumerRecord<?, ?> record, Acknowledgment ack,
+                                    @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+        Optional message = Optional.ofNullable(record.value());
+        if (message.isPresent()) {
+            Object msg = message.get();
+            try {
+                //业务逻辑处理在这里
+
+                //手动提交
+                ack.acknowledge();
+            }catch (Exception e){
+                log.warn("失败：{}",ExceptionUtils.getStackTrace(e));
             }
         }
     }
--- xin-demo/xin-commons-multi/xin-commons-multi-kafka/src/main/java/com/xin/commons/multi/kafka/producer/Producer.java
+++ xin-demo/xin-commons-multi/xin-commons-multi-kafka/src/main/java/com/xin/commons/multi/kafka/producer/Producer.java
@@ -4,7 +4,10 @@ import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 /**
  * kafkaTemplate2 表示使用第二个kafka数据源
@@ -29,6 +32,28 @@ public class Producer {
         kafkaTemplate1.send(TPOIC,object);
         //发送给kafka2
         kafkaTemplate2.send(TPOIC,object);
+    }
+
+    /**
+     * 发送消息,失败和返回后的处理
+     * @param obj
+     */
+    public void send(JSONObject obj) {
+        ListenableFuture<SendResult<String, Object>> future = kafkaTemplate1.send(TPOIC, obj);
+        future.addCallback(new ListenableFutureCallback<SendResult<String, Object>>() {
+            @Override
+            public void onFailure(Throwable throwable) {
+                //发送失败的处理
+                log.info(TPOIC + " - 生产者 发送消息失败：" + throwable.getMessage());
+
+            }
+
+            @Override
+            public void onSuccess(SendResult<String, Object> stringObjectSendResult) {
+                //成功的处理
+                log.info(TPOIC + " - 生产者 发送消息成功：" + stringObjectSendResult.toString());
+            }
+        });
     }
 
 }
